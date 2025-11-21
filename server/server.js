18ecf0d1bb07225801f5827ed7dc0b7f4109e7c5
harsh jaiswal <tech.harsh2003@gmail.com>
2025-11-22 00:17:31 +0530

    3rd deployment

--- server/server.js
+++ server/server.js
@@ -18,7 +18,9 @@ const port = process.env.PORT || 3000;
 
 app.use(cors({
     origin: process.env.CLIENT_URL,
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(httpLogger);
 app.use(cookieParser());
